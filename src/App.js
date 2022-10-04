83166d3c38eebe2a32bd10cf79b9f57d8a39a12c
pythagoras58 <solomondanso58@gmail.com>
2022-10-04 06:57:04 +0000

    morning commit - store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const counter = useSelector((state)=>state.counter)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>counter app</h1>
+      <h2>{counter}</h2>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore} from "redux";
+
+const reducerFn = (state = {counter:0}, action) =>{
+    return state;
+}
+
+const store = createStore(reducerFn);
+
+export default store;
